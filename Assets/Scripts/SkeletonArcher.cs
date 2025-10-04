f01de2d07eee2d55bf0f2deeaf2f96280118a842
lucasfreyess <lfreyes1@miuandes.cl>
2025-10-04 00:11:35 -0300

    feat: SkeletonArcher completado, junto con Prefab de Arrow para poder instanciarla y eliminarla on-the-fly

---
+++ Assets/Scripts/SkeletonArcher.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class SkeletonArcher : MonoBehaviour
+{
+    [SerializeField] private GameObject arrowPrefab;     // prefab para generar la flecha 
+    [SerializeField] private float StartingX = 8f;       // posicion fija en X del esqueleto
+    [SerializeField] private float StartingY = 0f;       // posicion fija en Y del esqueleto
+    [SerializeField] private float shootInterval = 3f;   // no puede ser igual que la velocidad del Gazer
+    [SerializeField] private float arrowAreaRadius = 3f; // area alrededor del esqueleto donde puede aparecer la flecha
+
+    private float shootIntervalTimer = 0f;
+    private GameObject currentArrow;                     // para poder destruirla cuando se spawnee otra jejjeje
+
+    
+    void Start()
+    {
+        // SkeletonArcher empieza en (8,0) y no se mueve
+        transform.position = new Vector2(StartingX, StartingY);
+    }
+
+    void Update()
+    {
+        UpdateState();
+    }
+
+    void UpdateState()
+    {
+        shootIntervalTimer += Time.deltaTime;
+        if (shootIntervalTimer >= shootInterval) // cuando el timer llega a 3 segundos, se dispara una flecha
+        {
+            Debug.Log("Timer excedio el intervalo!");
+            ShootArrow();
+            shootIntervalTimer = 0f; // reiniciar el timer
+        }
+    }
+
+    void ShootArrow()
+    {
+        if (currentArrow != null) Destroy(currentArrow);
+
+        // se crea una flecha entre la posicion del SkeletonArcher y el area definida por arrowAreaRadius
+        float arrowPositionY = Random.Range(transform.position.y - arrowAreaRadius, transform.position.y + arrowAreaRadius);
+        float arrowPositionX = Random.Range(transform.position.x - arrowAreaRadius, transform.position.x + arrowAreaRadius);
+
+        currentArrow = Instantiate(arrowPrefab, new Vector2(arrowPositionX, arrowPositionY), Quaternion.identity);
+
+        Debug.Log("Flecha creada en: " + currentArrow.transform.position);
+    }
+}
